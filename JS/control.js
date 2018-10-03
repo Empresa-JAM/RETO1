c1644882fc8fcdacbcfd9bb338a4e66e0ede4fc4
Unknown <alexsandro.ruiz@ikasle.egibide.org>
2018-10-04 01:53:49 +0200

    configuracion botones
    configuracion de botones marcha pause stop y meno de posiciones

--- JS/control.js
+++ JS/control.js
@@ -21,6 +21,50 @@ function init() {
 function moverPos() {
     //obtenemos el valor de la lista 'listaposiciones' que se corresponde con la posición a la que se quiere ir
     let pos=document.getElementById("listaPosiciones").value;
+
+    switch (parseInt(pos)){
+        case 1:
+            document.getElementById("milimetros").value = 50;
+            enviarMM("enviarMM");
+            break;
+        case 2:
+            document.getElementById("milimetros").value = 100;
+            enviarMM("enviarMM");
+            break;
+        case 3:
+            document.getElementById("milimetros").value = 150;
+            enviarMM("enviarMM");
+            break;
+        case 4:
+            document.getElementById("milimetros").value = 200;
+            enviarMM("enviarMM");
+            break;
+        case 5:
+            document.getElementById("milimetros").value = 250;
+            enviarMM("enviarMM");
+            break;
+        case 6:
+            document.getElementById("milimetros").value = 300;
+            enviarMM("enviarMM");
+            break;
+        case 7:
+            document.getElementById("milimetros").value = 350;
+            enviarMM("enviarMM");
+            break;
+        case 8:
+            document.getElementById("milimetros").value = 400;
+            enviarMM("enviarMM");
+            break;
+        case 9:
+            document.getElementById("milimetros").value = 450;
+            enviarMM("enviarMM");
+            break;
+        case 10:
+            document.getElementById("milimetros").value = 500;
+            enviarMM("enviarMM");
+            break;
+    }
+
     //el -1 se da cuando se selecciona el 'elige una posición
     //Se ha decidido no hacer nada en este caso
     if(pos!=-1){
