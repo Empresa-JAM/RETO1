8f16d4e813c6ad672781f569d13dead4eb95d21f
MikelFerreiro <43262935+MikelFerreiro@users.noreply.github.com>
2018-10-09 21:27:50 +0200

    Commit (supervisado)

--- JS/control.js
+++ JS/control.js
@@ -1,41 +1,53 @@
+
 /**
  * Created by Mikel on 02/10/2018.
  */
+
+
 //Esta funcion es principalmente para reajustar el bloque de posición cuando se reajusta la pantalla
 window.onresize = resize;
-function resize()
-{
+function resize() {
     if(document.getElementById("listaPosiciones").value!=-1)
         moverPos();
 }
+
 window.onload = function () {
     init();
 }
 
 function init() {
+    //abrir la modal
+    document.getElementById('modalOn').style.display="block";
+
     const bloque=document.getElementById("bloque");
 
 }
 
+function cerrarModal() {
+    document.getElementById('modalOn').style.display = "none";
+}
 
 function moverPos() {
     //obtenemos el valor de la lista 'listaposiciones' que se corresponde con la posición a la que se quiere ir
     let pos=document.getElementById("listaPosiciones").value;
+
     //el -1 se da cuando se selecciona el 'elige una posición
     //Se ha decidido no hacer nada en este caso
-    if(pos!=-1){
-        bloque.style.marginLeft=bloque.offsetWidth*pos+"px";
-    }
+      if(pos!=-1){
+          //bloque.style.marginLeft=bloque.offsetWidth*pos+"px";
+          var mm=(bloque.offsetWidth*pos*50)/bloque.offsetWidth;
+          guardarEstadisticas(parseInt(mm));
+          request2serverPulsador('"web".Posiciones por milimetros', mm);
+      }
 }
+
 
 function moverMm (){
     //obtenemos los milimetros a desplazarse
     let mm=document.getElementById("milimetros").value;
-    //calculamos la anchura total del rail o regla
-    let anchura=bloque.offsetWidth*10;
-    //mediante una regla de tres convertimos los mm a pixels
-    bloque.style.marginLeft=(anchura*mm)/500+"px";
+    guardarEstadisticas(parseInt(mm));
 }
+
 
 function automatico() {
     let auto = document.getElementById("automatico");
@@ -77,3 +89,4 @@ function manual() {
     document.getElementById("pausa").setAttribute("disabled","disabled");
     document.getElementById("stop").setAttribute("disabled","disabled");
 }
+
